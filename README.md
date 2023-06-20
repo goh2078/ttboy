28e4be23531e7677e014c4fda27de6effc1dbc74
smarterlighter <smarterlighter@outlook.com>
2023-06-20 16:50:04 +0800

    first commit lucky

---
+++ README.md
@@ -0,0 +1,3 @@
+# ttboy
+# ttboy
+# ttboy
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+//$host = "127.0.0.1";
+$host = "localhost";
+$username = "scarletpal2378";
+$password = "Ff2400#255360";
+$dbname = "sex_quiz";
+$mysqli = new mysqli($host, $username, $password, $dbname);
+?>
